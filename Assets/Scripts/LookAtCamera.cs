976f11b90c33e0ff071f9009e24b0b330f0a5934
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2017-07-28 23:23:40 -0300

    Basic project settings

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LookAtCamera : MonoBehaviour {
+
+  Camera cam;
+
+  // Use this for initialization
+  void Start() {
+    cam = FindObjectOfType<Camera>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    transform.localRotation = cam.transform.rotation;
+  }
+}
