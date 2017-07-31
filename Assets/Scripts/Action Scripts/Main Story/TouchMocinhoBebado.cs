e772a9484a31c1afc5c9590417e425a324bbb9ab
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2017-07-31 12:17:48 -0300

    asd

---
+++ Assets/Scripts/Action Scripts/Main Story/TouchMocinhoBebado.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMocinhoBebado : MoveToTouchScript {
+  protected override IEnumerator TouchRoutine() {
+    gm.StartTalking();
+    yield return gm.Talk("He's too nervous for a hug.", gm.Bro);
+    gm.StopTalking();
+  }
+}
