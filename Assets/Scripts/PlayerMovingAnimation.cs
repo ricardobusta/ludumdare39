896f1bd93025dfea9c68dfa5832b92db7302e0df
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2017-07-30 21:35:58 -0300

    Adding player walking animation

---
+++ Assets/Scripts/PlayerMovingAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovingAnimation : MonoBehaviour {
+
+  public Animator animator;
+  public Character character;
+
+  void Update() {
+    animator.SetBool("Moving", character.move);
+  }
+}
